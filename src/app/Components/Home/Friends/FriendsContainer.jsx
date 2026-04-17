5f065bf69dca11795d60fed285034c9cb33b71d0
md-sh-shuvo <shuvo.avail@gmail.com>
2026-04-17 16:51:04 +0600

    background color change functionality of status in friend card added

--- src/app/Components/Home/Friends/FriendsContainer.jsx
+++ src/app/Components/Home/Friends/FriendsContainer.jsx
@@ -18,8 +18,8 @@ const FriendsContainer = () => {
                             <Image src={`${friend.picture}`} width='80' height='80' alt="" className="rounded-full mx-auto"></Image>
                             <h1 className="text-[#1F2937] text-xl font-semibold">{friend.name}</h1>
                             <p className="text-[#64748B] text-[12px]">{friend.days_since_contact }d ago</p>
-                            <p className="flex gap-2 justify-center items-center my-[6px]">{friend.tags.map((tag, idx) => <span key={idx} className="bg-[#CBFADB] text-[#244D3F] text-[12px] font-medium py-1 px-3 rounded-full">{tag}</span>)}</p>
-                            <span className="bg-[#EF4444] py-1 px-3 text-white rounded-full text-[12px] font-medium">{friend.status}</span>
+                            <p className="flex gap-2 justify-center items-center my-1.5">{friend.tags.map((tag, idx) => <span key={idx} className="bg-[#CBFADB] text-[#244D3F] text-[12px] font-medium py-1 px-3 rounded-full">{tag}</span>)}</p>
+                            <span className={`${friend.status === 'almost due' ? "bg-[#EFAD44]" : friend.status === 'overdue' ? "bg-[#EF4444]" : "bg-[#244D3F]"}  py-1 px-3 text-white rounded-full text-[12px] font-medium`}>{friend.status}</span>
                         </div>
                     </div>
                 )
