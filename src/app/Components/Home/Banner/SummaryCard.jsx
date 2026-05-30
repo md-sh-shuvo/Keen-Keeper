07971a0fa54c7961ebe63e443be81a13bd3d5925
md-sh-shuvo <shuvo.avail@gmail.com>
2026-05-30 20:47:02 +0600

    Navbar issues fixed

--- src/app/Components/Home/Banner/SummaryCard.jsx
+++ src/app/Components/Home/Banner/SummaryCard.jsx
@@ -1,21 +1,25 @@
+import friendsData from '@/data/friends.json'
+
 
 const SummaryCard = () => {
+    const onTrack = friendsData.filter(friend => friend.status === 'on-track')
+    const overdue = friendsData.filter(friend => friend.status === 'overdue')
     return (
         <div className="md:grid grid-cols-2 lg:grid-cols-4 gap-6 max-w-[70%] mx-auto mt-10"> 
             <div className="bg-white rounded-lg shadow-lg p-8">
-                <h1 className="text-[#244D3F] text-3xl font-semibold">10</h1>
+                <h1 className="text-[#244D3F] text-3xl font-semibold">{friendsData.length}</h1>
                 <p className="text-[#64748B]">Total Friends</p>
             </div>
             <div className="bg-white rounded-lg shadow-lg p-8">
-                <h1 className="text-[#244D3F] text-3xl font-semibold">10</h1>
+                <h1 className="text-[#244D3F] text-3xl font-semibold">{onTrack.length}</h1>
                 <p className="text-[#64748B]">On Track</p>
             </div>
             <div className="bg-white rounded-lg shadow-lg p-8">
-                <h1 className="text-[#244D3F] text-3xl font-semibold">10</h1>
+                <h1 className="text-[#244D3F] text-3xl font-semibold">{overdue.length}</h1>
                 <p className="text-[#64748B]">Need Attention</p>
             </div>
             <div className="bg-white rounded-lg shadow-lg p-8">
-                <h1 className="text-[#244D3F] text-3xl font-semibold">10</h1>
+                <h1 className="text-[#244D3F] text-3xl font-semibold">12</h1>
                 <p className="text-[#64748B]">Interactions This Months</p>
             </div>
         </div>
--- src/app/Components/Navbar/Navbar.jsx
+++ src/app/Components/Navbar/Navbar.jsx
@@ -29,27 +29,30 @@ const Navbar = () => {
 
 
     return (
-        <div className="navbar max-w-7xl mx-auto">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-                    </div>
-                    <ul
-                        tabIndex="-1"
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        {links}
 
+        <div className="bg-white border-b-2 border-gray-100">
+            <div className="navbar max-w-7xl mx-auto">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex="-1"
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            {links}
+
+                        </ul>
+                    </div>
+                    <Link href='/' className="text-2xl"><span className="font-bold text-[#1F2937]">Keen</span><span className="text-[#1F2937]">Keeper</span> </Link>
+                </div>
+                <div className="navbar-end hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {links}
                     </ul>
                 </div>
-                <Link href='/' className="text-2xl"><span className="font-bold text-[#1F2937]">Keen</span><span className="text-[#1F2937]">Keeper</span> </Link>
-            </div>
-            <div className="navbar-end hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    {links}
-                </ul>
-            </div>
 
+            </div>
         </div>
     );
 };
